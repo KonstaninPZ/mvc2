f33343ecd1c2e7103e1d0902de1997e193f9b91c
Konstantin Shikhov <kon-shikhov@yandex.ru>
2017-12-07 00:45:07 +0300

    add smarty to project

--- config/config.php
+++ config/config.php
@@ -1,6 +1,29 @@
 <!--//файл для обьявления констант -->
 <?php
-//крнстанты для обращения к контроллерам
+// Константы для обращения к контроллерам
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
 
+// Используемый шаблон
+$template = 'default';
+
+// Пути к файлам шаблонов (*.tpl)
+define('TemplatePrefix', "../views/{$template}/");
+define('TemplatePostFix', '*.tpl');
+
+// Пути к файлам шаблонов в вебпространстве(www) это js,css файлы
+define('TemplateWebPath', "/templates/{$template}/");
+
+// Инициализация шаблонизатора Smarty
+// put full path to Smarty.class.php
+require ('../library/Smarty/libs/Smarty.class.php');
+
+$smarty = new Smarty();
+
+$smarty -> setTemplateDir(TemplatePrefix);
+$smarty -> setCompileDir('../tmp/smarty/templates_c');
+$smarty -> setCacheDir('../tmp/smarty/cache');
+$smarty -> setConfigDir('../library/Smarty/cofigs');
+
+// щбъявление переменной с помощью метода assign
+$smarty -> assign('templateWebPath',TemplateWebPath);
